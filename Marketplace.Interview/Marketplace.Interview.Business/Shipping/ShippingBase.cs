e177133f620b15149bf5100ab3ff561d70ebbe46
tukhoi <tukhoi@gmail.com>
2015-05-26 16:31:59 +0700

    Revert "interview"
    This reverts commit d3482780713e4441eb98892cf52c3b07182632ba.

--- Marketplace.Interview/Marketplace.Interview.Business/Shipping/ShippingBase.cs
+++ Marketplace.Interview/Marketplace.Interview.Business/Shipping/ShippingBase.cs
@@ -10,13 +10,11 @@ namespace Marketplace.Interview.Business.Shipping
     {
         public static IEnumerable<Type> KnownTypes()
         {
-            return new[] {typeof (FlatRateShipping), typeof (PerRegionShipping), typeof(CustomPerRegionShipping)};
+            return new[] {typeof (FlatRateShipping), typeof (PerRegionShipping)};
         }
         
         public abstract string GetDescription(LineItem lineItem, Basket.Basket basket);
 
         public abstract decimal GetAmount(LineItem lineItem, Basket.Basket basket);
-
-        public string Name { get; protected set; }
     }
 }
