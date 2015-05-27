1c35f2fc65aa7843d82aa67611493389cae09352
tukhoi <tukhoi@gmail.com>
2015-05-27 12:48:18 +0700

    Revert "Revert "interview""
    This reverts commit e177133f620b15149bf5100ab3ff561d70ebbe46.

---
+++ Marketplace.Interview/Marketplace.Interview.Business/Shipping/CustomRegionShippingCost.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marketplace.Interview.Business.Shipping
+{
+    public class CustomRegionShippingCost : RegionShippingCost
+    {
+        public decimal DeductedAmount { get; set; }
+    }
+}
