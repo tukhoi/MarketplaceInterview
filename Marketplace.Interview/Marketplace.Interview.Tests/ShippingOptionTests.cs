7cbd8f046a082eb4604af658f794f9e8a35f3b11
tukhoi <tukhoi@gmail.com>
2015-05-27 15:57:52 +0700

    Revert "Revert "interview""
    This reverts commit e177133f620b15149bf5100ab3ff561d70ebbe46.

--- Marketplace.Interview/Marketplace.Interview.Tests/ShippingOptionTests.cs
+++ Marketplace.Interview/Marketplace.Interview.Tests/ShippingOptionTests.cs
@@ -94,5 +94,107 @@ namespace Marketplace.Interview.Tests
 
             Assert.That(basketShipping, Is.EqualTo(3.35m));
         }
+
+        [Test]
+        public void CustomPerRegionShippingOptionTest()
+        {
+            var customPerRegionShippingOption = new CustomPerRegionShipping()
+            {
+                PerRegionCosts = new[]
+                                                                       {
+                                                                           new CustomRegionShippingCost()
+                                                                               {
+                                                                                   DestinationRegion =
+                                                                                       RegionShippingCost.Regions.UK,
+                                                                                   Amount = .75m,
+                                                                                   DeductedAmount = .05m
+                                                                               },
+                                                                           new CustomRegionShippingCost()
+                                                                               {
+                                                                                   DestinationRegion =
+                                                                                       RegionShippingCost.Regions.Europe,
+                                                                                   Amount = 1.5m,
+                                                                                   DeductedAmount = .05m
+                                                                               }
+                                                                       },
+            };
+
+            var shippingAmount = customPerRegionShippingOption.GetAmount(new LineItem() { DeliveryRegion = RegionShippingCost.Regions.Europe }, new Basket());
+            Assert.That(shippingAmount, Is.EqualTo(1.5m));
+
+            shippingAmount = customPerRegionShippingOption.GetAmount(new LineItem() { DeliveryRegion = RegionShippingCost.Regions.UK }, new Basket());
+            Assert.That(shippingAmount, Is.EqualTo(.75m));
+        }
+
+        [Test]
+        public void BasketCustomShippingTotalTest()
+        {
+            var customPerRegionShippingOption = new CustomPerRegionShipping()
+            {
+                PerRegionCosts = new[]
+                    {
+                        new CustomRegionShippingCost()
+                            {
+                                DestinationRegion =
+                                    RegionShippingCost.Regions.UK,
+                                Amount = .75m,
+                                DeductedAmount = .05m
+                            },
+                        new CustomRegionShippingCost()
+                            {
+                                DestinationRegion =
+                                    RegionShippingCost.Regions.Europe,
+                                Amount = 1.5m,
+                                DeductedAmount = .05m
+                            }
+                    },
+            };
+
+            var flatRateShippingOption = new FlatRateShipping { FlatRate = 1.1m };
+
+            var basket = new Basket()
+            {
+                LineItems = new List<LineItem>
+                    {
+                        new LineItem()
+                            {
+                                DeliveryRegion = RegionShippingCost.Regions.UK,
+                                SupplierId = 1,
+                                Shipping = customPerRegionShippingOption
+                            },
+                        new LineItem()
+                            {
+                                DeliveryRegion = RegionShippingCost.Regions.Europe,
+                                SupplierId = 1,
+                                Shipping = customPerRegionShippingOption
+                            },
+                        new LineItem() {Shipping = flatRateShippingOption},
+                        new LineItem()
+                            {
+                                DeliveryRegion = RegionShippingCost.Regions.UK,
+                                SupplierId = 1,
+                                Shipping = customPerRegionShippingOption
+                            },
+                            new LineItem()
+                            {
+                                DeliveryRegion = RegionShippingCost.Regions.UK,
+                                SupplierId = 2,
+                                Shipping = customPerRegionShippingOption
+                            },
+                    }
+            };
+
+            var calculator = new ShippingCalculator();
+
+            decimal basketShipping = calculator.CalculateShipping(basket);
+
+            Assert.AreEqual(0.7m, basket.LineItems[0].ShippingAmount);
+            Assert.AreEqual(1.5m, basket.LineItems[1].ShippingAmount);
+            Assert.AreEqual(1.1m, basket.LineItems[2].ShippingAmount);
+            Assert.AreEqual(0.7m, basket.LineItems[3].ShippingAmount);
+            Assert.AreEqual(0.75m, basket.LineItems[4].ShippingAmount);
+            Assert.That(basketShipping, Is.EqualTo(4.75m));
+        }
+
     }
 }
