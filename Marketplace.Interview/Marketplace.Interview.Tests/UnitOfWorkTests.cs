b7f53a2b8efe2697d8ecc99c02391fd01c48ff67
tukhoi <tukhoi@gmail.com>
2015-05-28 08:53:12 +0700

    Tests

---
+++ Marketplace.Interview/Marketplace.Interview.Tests/UnitOfWorkTests.cs
@@ -0,0 +1,89 @@
+﻿using Marketplace.Interview.Business.Basket;
+using Marketplace.Interview.Business.Core.UnitOfWork;
+using Marketplace.Interview.Business.Shipping;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marketplace.Interview.Tests
+{
+    [TestFixture]
+    public class UnitOfWorkTests
+    {
+        [SetUp]
+        public void TestStart()
+        { }
+
+        [TearDown]
+        public void TestEnd()
+        {
+            string file = Path.Combine(Environment.GetEnvironmentVariable("temp"), "basket.xml");
+            if (File.Exists(file))
+                File.Delete(file);
+        }
+
+        [Test]
+        public void CommitUnitOfWorkTest()
+        {
+            var unitOfWork = UnitOfWorkFactory.Create();
+            var readBasketCommand = new GetBasketQuery(unitOfWork);
+            var oldBasket = readBasketCommand.Invoke(new BasketRequest());
+            var addBasketCommand = new Marketplace.Interview.Business.Basket.AddToBasketCommand(unitOfWork);
+
+            var newLineItem = new LineItem()
+            {
+                Amount = 1,
+                DeliveryRegion = RegionShippingCost.Regions.UK,
+                ProductId = "111111",
+                SupplierId = 1,
+                Shipping = new FlatRateShipping() { FlatRate = 1.5m }
+            };
+
+            addBasketCommand.Invoke(new AddToBasketRequest()
+            {
+                LineItem = newLineItem
+            });
+
+            unitOfWork.Commit();
+            var newBasket = readBasketCommand.Invoke(new BasketRequest());
+            Assert.That(oldBasket.LineItems.Count < newBasket.LineItems.Count);
+            
+            var persistItem = newBasket.LineItems.Last();
+            Assert.That(newLineItem.ProductId == persistItem.ProductId);
+        }
+
+        [Test]
+        public void RollbackUnitOfWorkTest()
+        {
+            var unitOfWork = UnitOfWorkFactory.Create();
+            var readBasketCommand = new GetBasketQuery(unitOfWork);
+            var oldBasket = readBasketCommand.Invoke(new BasketRequest());
+            var addBasketCommand = new Marketplace.Interview.Business.Basket.AddToBasketCommand(unitOfWork);
+
+            var newLineItem = new LineItem()
+            {
+                Amount = 1,
+                DeliveryRegion = RegionShippingCost.Regions.UK,
+                ProductId = "222222",
+                SupplierId = 1,
+                Shipping = new FlatRateShipping() { FlatRate = 1.5m }
+            };
+            addBasketCommand.Invoke(new AddToBasketRequest()
+            {
+                LineItem = newLineItem
+            });
+
+            unitOfWork.Rollback();
+
+            var newBasket = readBasketCommand.Invoke(new BasketRequest());
+            Assert.AreEqual(oldBasket.LineItems.Count, newBasket.LineItems.Count);
+            
+            var saved = oldBasket.LineItems.Any(x => x.ProductId == newLineItem.ProductId);
+            Assert.That(saved == false);
+        }
+    }
+}
